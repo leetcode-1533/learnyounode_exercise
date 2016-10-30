ee5f50c463f2dec215ba0b9c5e7c1b2ee1998697
Yingjie <1275963@gmail.com>
2016-10-30 16:01:27 -0400

    Http collect
    Use module bl for handling the http stream

--- program.js
+++ program.js
@@ -54,10 +54,23 @@ Filtered LS
 /*
     Http Request
  */
+// var http = require('http')
+// http.get(process.argv[2], function(response){
+//     response.setEncoding("utf8")
+//     response.on("data", function(data) {
+//         console.log(data);
+//     });
+// });
+
+/*
+    Http collect
+ */
+var bl = require('bl')
 var http = require('http')
-http.get(process.argv[2], function(response){
-    response.setEncoding("utf8")
-    response.on("data", function(data) {
-        console.log(data);
-    });
-});
+
+http.get(process.argv[2], function(response) {
+    response.pipe(bl(function(err, data){
+        console.log(data.length);
+        console.log(data.toString())
+    }))
+})
