186bcec70048b9935a3f8540f78163588ef97bf3
Yingjie <1275963@gmail.com>
2016-11-05 22:44:16 -0400

    url.test to set different entry point

--- program.js
+++ program.js
@@ -137,12 +137,51 @@ Http file server
 /*
 Http uppercase server
  */
+// var http = require('http')
+// var map = require('through2-map')
+// var callback = function(request, response) {
+//     request.pipe(map(function (chunk) {
+//         return chunk.toString().toUpperCase()
+//     })).pipe(response)
+// }
+// var server = http.createServer(callback)
+// server.listen(process.argv[2])
+
+/*
+http json api server
+ */
+// var http = require('http')
 var http = require('http')
-var map = require('through2-map')
-var callback = function(request, response) {
-    request.pipe(map(function (chunk) {
-        return chunk.toString().toUpperCase()
-    })).pipe(response)
+var url = require('url')
+
+function parsetime (time) {
+    return {
+        hour: time.getHours(),
+        minute: time.getMinutes(),
+        second: time.getSeconds()
+    }
 }
-var server = http.createServer(callback)
-server.listen(process.argv[2])
+
+function unixtime (time) {
+    return { unixtime : time.getTime() }
+}
+
+var server = http.createServer(function (req, res) {
+    var parsedUrl = url.parse(req.url, true)
+    var time = new Date(parsedUrl.query.iso)
+    var result
+
+    if (/^\/api\/parsetime/.test(req.url))
+        result = parsetime(time)
+    else if (/^\/api\/unixtime/.test(req.url))
+        result = unixtime(time)
+
+    if (result) {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(result))
+    } else {
+        res.writeHead(404)
+        res.end()
+    }
+})
+server.listen(Number(process.argv[2]))
