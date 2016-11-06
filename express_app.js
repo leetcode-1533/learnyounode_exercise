f50e3faac1d4975cb81ad265a3b4b9bd62f0e161
Yingjie <1275963@gmail.com>
2016-11-05 23:06:20 -0400

    learning express in nodejs

---
+++ express_app.js
@@ -0,0 +1,19 @@
+/**
+ * Created by y1275963 on 11/5/16.
+ */
+var express = require('express')
+var app = express()
+app.get('/', function(req, res) {
+    res.send('Hello World')
+})
+
+app.get('/musician/:name', function(req, res) {
+
+    // Get /musician/Matt
+    console.log(req.params.name)
+    // => Matt
+    res.writeheadq
+    res.send('{"id": 1,"name":"Matt", "band":"BBQ Brawlers"}');
+});
+
+app.listen(8000)
